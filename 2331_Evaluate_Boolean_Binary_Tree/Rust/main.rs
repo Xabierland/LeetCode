860d069ca42eb1a819486a7e6d6c4f6b1f155955
Xabierland <xabierland@gmail.com>
2024-05-16 17:46:38 +0200

    update

---
+++ 2331_Evaluate_Boolean_Binary_Tree/Rust/main.rs
@@ -0,0 +1,22 @@
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn evaluate_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Soluciones a problemas de LeetCode.
 
 Mi perfil: [LeetCode](https://leetcode.com/u/Xabierland/)
+
+## Problemas sin resolver
+
+- 21. Merge Two Sorted Lists
+- 420. Strong Password Checker
+- 2331. Evaluate Boolean Binary Tree
